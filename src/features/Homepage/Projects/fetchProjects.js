4615c7a9e229b5f0052e90c836d8aeacd3e330b8
Przemysław Pacek <ppacek@op.pl>
2024-11-01 09:38:10 +0100

    added longer loading time

--- src/features/Homepage/Projects/fetchProjects.js
+++ src/features/Homepage/Projects/fetchProjects.js
@@ -1,6 +1,8 @@
 import { data } from "../../../common/data/data";
 
 export const fetchProjects = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
   const response = await fetch(`${data.githubAPI}`);
 
   if (!response.ok) {
